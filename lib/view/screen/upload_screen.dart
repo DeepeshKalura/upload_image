65a4c13c6365efb8e72dc8f7c749aa2e3d2b3eea
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-03-18 23:28:22 +0530

    Design the upload screen
    Here I have made UI parts of the upload screen. I have used the following widgets:
    Container in which we are going to show the image preview
    Text with provider package
    Button which will upload the image

--- lib/view/screen/upload_screen.dart
+++ lib/view/screen/upload_screen.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
+import '../widget/rounded_widget.dart';
+
 
 class UploadScreen extends StatefulWidget {
   const UploadScreen({super.key});
@@ -12,20 +14,51 @@ class UploadScreen extends StatefulWidget {
 
 class _UploadScreenState extends State<UploadScreen> {
   File? isImage;
-
+  String situationTextNotifier = 'Upload Your Document';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            child: isImage == null ? const Icon(
-              Icons.upload_file_outlined,
-              size: 100,
-            ) : Image.file(isImage!),
-          )
+    return Container(
+      decoration:  const BoxDecoration(
+       gradient: LinearGradient(
+        begin: Alignment.bottomLeft,
+        end: Alignment.topRight,
+        stops: [0.0, 1.0],
+        colors: [
+          Color.fromARGB(255, 27, 26, 26),
+          Colors.white,
         ],
+      ),
+    ),
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  child: isImage == null ? const Icon(
+                    Icons.upload_file_outlined,
+                    size: 100,
+                    color: Colors.white,
+                  ) : Image.file(isImage!,),
+                ),
+              ),
+                Text(situationTextNotifier),
+              RoundedButtonWidget(
+                onpressed: () { 
+                },
+              
+                width: MediaQuery.of(context).size.width * 0.8,
+                buttonText: 'Slected',
+                // child: const Text('Select'),
+              ),
+              
+            ],
+          ),
+        ),
       ),
     );
   }
