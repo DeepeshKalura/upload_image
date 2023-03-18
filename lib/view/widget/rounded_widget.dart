0a16a0528fe0344b3ef8a2384267948385013881
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-03-18 23:25:31 +0530

    Button Design:
    I have Just completed the button Design which is given by the assisiment
    
    It has gradient color and less rounded corner My guess was the 10.

---
+++ lib/view/widget/rounded_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class RoundedButtonWidget extends StatelessWidget {
+  final String buttonText;
+  final double width;
+  final Function onpressed;
+
+  const RoundedButtonWidget({super.key, 
+    required this.buttonText,
+    required this.width,
+    required this.onpressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        decoration: BoxDecoration(
+          
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.0, 1.0],
+            colors: [
+              Colors.red.shade900,
+              Colors.orange.shade900,
+            ],
+          ),
+          // color: Colors.orange.shade300,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ElevatedButton(
+          style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              minimumSize: MaterialStateProperty.all(Size(width, 50)),
+              
+              elevation: MaterialStateProperty.all(0),
+          ),
+          onPressed: () {
+            onpressed();
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 10,
+              bottom: 10,
+            ),
+            child: Text(
+              buttonText,
+              style: TextStyle(
+                fontSize: 18,
+                // fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
